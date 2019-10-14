acae797e40aff7641d388d30a702aef46d87f61d
orvly <o@o>
2019-10-14 23:09:25 +0300

    Added second "solution" for nth prime. Done by using vibe to get the least of primes from an online source, and searching them using regex to find the nth one

--- nth-prime/source/nth_prime.d
+++ nth-prime/source/nth_prime.d
@@ -1,6 +1,6 @@
 module nth_prime;
 
-ulong prime(ulong nth) {
+ulong prime1(ulong nth) {
     // Method: Sieve of Erastothenes.
 
     import std.container.array: Array;
@@ -30,6 +30,56 @@ ulong prime(ulong nth) {
         lastKnownPrime = isPrime[].drop(lastKnownPrime + 1).countUntil(false) + lastKnownPrime + 1;
     }
     return lastKnownPrime;
+}
+
+ulong prime(const ulong nth) {
+    import vibe.http.client;
+    import vibe.stream.operations;
+    import std.conv;
+    import std.regex;
+    import std.algorithm;
+    import std.string;
+    import std.exception: enforce;
+
+    enforce(nth > 0, "illegal input");
+
+    ulong p;
+    void handleResponse(scope HTTPClientResponse resp) {
+        auto reader = resp.bodyReader;
+        auto r = regex(`\d+`, "g");
+        foreach(i; 0..4) { // Skip heading - first 4 lines
+            reader.readLine;
+        }
+        uint num = 0;
+        while(!reader.empty) {
+            // assumeUTF converts to a string
+            auto line = reader.readLine.assumeUTF;
+            auto matches = line.matchAll(r);
+            // matches is a lazy range, so we also need a function that both knows where to stop
+            // iterating, AND returns the value of the place where we stopped.  find() is such
+            // a function, but we misuse it here, we aren't really interested in finding anything,
+            // just using it as a loop, so the actual start and values don't matter.
+            // We could have also used each() which is the more correct function in this case,
+            // but that would actually be more cumbersome to write...
+
+            // BUG: If "num" isn't defined, then the following line will fail to compile,
+            //      but with the wrong error message about a mismatching template instead of "unknown symbol".
+            auto found = matches.find!((_,__) => ++num == nth)(typeof(matches.front).init);
+            // find() returns a sub-range which starts at the found location, so we
+            // just have to use front() on it to get to the match itself.
+            // hit() is used to get the actual text.
+            if (!found.empty) {
+                p = found.front.hit.to!ulong;
+                break;
+            }
+        }
+    }
+    requestHTTP("https://primes.utm.edu/lists/small/100000.txt",
+                (scope req) {
+                    req.method = HTTPMethod.POST;
+                },
+                &handleResponse);
+    return p;
 }
 
 unittest
